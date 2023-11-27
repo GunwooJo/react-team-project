773329d72d79639a8c39ca3a18e320b36055ab61
GunwooJo <kanggi1997@naver.com>
2023-11-27 17:13:04 +0900

    feat: 디렉토리 구조 나누기

---
+++ src/components/SampleComponents.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SampleComponents() {
+  return (
+    <div>샘플 컴포넌트 예시입니다.</div>
+  )
+}
+
+export default SampleComponents
---
+++ src/pages/SamplePage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SamplePage() {
+  return (
+    <div>샘플 페이지 예시입니다.</div>
+  )
+}
+
+export default SamplePage
