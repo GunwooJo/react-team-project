94c7900f7c1351dfb6251ab61f3a057a70debf04
Lacheln1 <psionict@naver.com>
2023-11-29 02:16:14 +0900

    데이터 뽑아쓰기 테스트

--- src/constants/Test01.js
+++ src/constants/Test01.js
@@ -8,16 +8,24 @@ export default function Test01(){
 
         let list = new Array();
         axios
-        .get('https://openapi.foodsafetykorea.go.kr/api/419cf02bcbf849288b6f/COOKRCP01/json/1/5/',{
-                params:{
-                        URL: 'RCP_PAT2=%EB%B0%98%EC%B0%AC'
-                }
+        .get('https://openapi.foodsafetykorea.go.kr/api/419cf02bcbf849288b6f/COOKRCP01/json/1/5/RCP_PAT2=반찬')
+        .then((response)=>{
+
+              for(var i=0;i<5;i++){
+                list[i]=response.data.COOKRCP01.row[i].RCP_WAY2;
+                console.log(i)
+                console.log(list[i]);
+                console.log("첫번째 종류"+list[0]);
+
+              }
+                
+                
+                
         })
-        .then((response)=>console.log(response.data))
         .catch(console.log);
 
         
- 
+        
        
 
    
