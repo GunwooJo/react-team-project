10f6174a2d0899bae4f54c94abf50e7321cbae3e
Lacheln1 <psionict@naver.com>
2023-11-30 20:29:18 +0900

    반찬 목록 가져오기

--- src/constants/Test01.js
+++ src/constants/Test01.js
@@ -1,39 +1,37 @@
 import axios from "axios";
 import React from "react";
-import { useState,useEffect } from "react";
-import { APIKEY } from './apidata';
+import { useState, useEffect } from "react";
+import { APIKEY } from "./apidata";
 
+export default function Test01() {
+  const [cookBoil, setCookBoil] = useState([]);
 
-export default function Test01(){
+  useEffect(() => {
+    const getData = async () => {
+      //비동기 안하면 데이터가 로딩되기 전에 렌더링이 완료되기 때문에 사용해줘야함
+      await axios
+        .get(
+          "https://openapi.foodsafetykorea.go.kr/api/419cf02bcbf849288b6f/COOKRCP01/json/1/565/RCP_PAT2=반찬"
+        )
+        .then((response) => {
+          let foodNameList = new Array();
+          for (let i = 0; i < 564; i++) {
+            const foodName = response.data.COOKRCP01.row[i].RCP_NM;
+            if (foodName === null) break;
 
-  // const [cookBoil,setCookBoil] = useState([]);
-  // const getData = async ()->{
-  //   axios
-  //   .get('https://openapi.foodsafetykorea.go.kr/api/419cf02bcbf849288b6f/COOKRCP01/json/1/5/RCP_WAY2=끄')
-
-  // }
-
-        let list = new Array();
-        axios
-        .get('https://openapi.foodsafetykorea.go.kr/api/419cf02bcbf849288b6f/COOKRCP01/json/1/5/RCP_PAT2=반찬')
-        .then((response)=>{
-
-              for(var i=0;i<5;i++){
-                list[i]=response.data.COOKRCP01.row[i].RCP_WAY2;
-                console.log(i)
-                console.log(list[i]);
-                console.log("첫번째 종류"+list[0]);
-
-              }
-                
-                
-                
+            foodNameList.push(foodName);
+            console.log(foodName);
+          }
+          setCookBoil(foodNameList);
         })
-        .catch(console.log);
+        .catch((error) => {
+          console.log(error);
+        });
+    };
+    getData(); // 비동기 작업을 수행하기 위해서 호출, useEffect 안에서 비동기 작업을 수행하면서 업데이트
+  }, []);
 
-        
-        
-       
 
-   
+  //데이터 활용 부분
+  return <div>{cookBoil[3]}</div>;
 }
--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import SamplePage from './pages/SamplePage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  
     <SamplePage />
-  </React.StrictMode>
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
