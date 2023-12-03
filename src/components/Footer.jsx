5e2f3d64f2ed83d69700eb19de90062e7791e8bb
ny030303 <51257615+ny030303@users.noreply.github.com>
2023-12-03 19:55:46 +0900

    add Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import { React, useState } from 'react';
+
+
+function Footer() {
+  return (
+   <div style={{width:"100%", height:"120px", backgroundColor:"#d9d9d9", position:"absolute", bottom:"0"}}>
+    <p style={{textAlign:"center", paddingTop:"80px"}}>Copyright KNU Student Inc. All Rights Reserved</p>
+   </div>
+  )
+}
+
+export default Footer
