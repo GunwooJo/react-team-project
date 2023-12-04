388a8e8aa3c67896d4022c7473d9511f8e7b4a9c
GunwooJo <kanggi1997@naver.com>
2023-12-05 03:05:09 +0900

    feat: 레시피 상세페이지 구성 완료 후 디자인 중

--- src/pages/RecipeDetailPage.jsx
+++ src/pages/RecipeDetailPage.jsx
@@ -15,7 +15,7 @@ function RecipeDetailPage() {
     try {
       const response = await axios.get(`${FOOD_SAFETY_KOREA_URL}/1/1000/RCP_NM=${RCP_NM}`);
       const recipeData = response.data.COOKRCP01.row[0];
-      
+
       setRecipeObj(recipeData);
       makeManualList(recipeData);
       makeManualImgList(recipeData);      
@@ -63,17 +63,39 @@ function RecipeDetailPage() {
   }, [])
 
   return (
-    <div>
+    <div style={{textAlign: 'center'}}>
+      
+      <img src={recipeObj.ATT_FILE_NO_MK} style={{width: '400px', marginTop: '40px', marginBottom: '20px'}} alt='레시피 대표 이미지'/>
+      <div style={{marginBottom: '30px'}}>
+        <div style={{color: '#555555', fontSize: '1.2rem'}}>{recipeObj.RCP_PAT2}</div>
+        <b style={{fontSize: '1.2rem'}}>{recipeObj.RCP_NM}</b>
+      </div>
+      
+
+      <div>
+        <b style={{color: '#555555', margin: 'auto 10px'}}>{recipeObj.RCP_WAY2}</b>
+        <b style={{color: '#555555', margin: 'auto 10px'}}>{recipeObj.INFO_ENG} kcal</b>
+        <b style={{margin: 'auto 3px'}}>탄수화물: {recipeObj.INFO_CAR}</b>
+        <b style={{margin: 'auto 3px'}}>단백질: {recipeObj.INFO_PRO}</b>
+        <b style={{margin: 'auto 3px'}}>지방: {recipeObj.INFO_FAT}</b>
+        <b style={{margin: 'auto 3px'}}>나트륨: {recipeObj.INFO_NA}</b>
+      </div>
+
+      <hr style={{width: '60vw', margin: '0 auto', marginBottom: '50px'}}/>
+      <b>{recipeObj.RCP_PARTS_DTLS}</b>
+
       {
         manualList.map((manual, idx) => {
           return (
             <div key={manual}>
-              <img src={manualImgList[idx]} alt='메뉴얼 이미지'/>
-              <p>{manual}</p>
+              <img src={manualImgList[idx]} alt='레시피 단계 이미지' />
+              <br/>
+              <b>{manual}</b>
             </div>
           )
         })
       }
+
     </div>  
   )
 }
