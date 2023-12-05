9244a3edc90dbf104aaa1a16940ea6a2cef1c541
Lacheln1 <psionict@naver.com>
2023-12-05 22:43:14 +0900

    레시피 삭제 기능
    레시피 삭제 기능 및 부트스트랩

--- src/pages/UserRecipeListPage.jsx
+++ src/pages/UserRecipeListPage.jsx
@@ -1,53 +1,56 @@
 import axios from "axios";
-import {React,useEffect,useState} from "react";   
-import Badge from 'react-bootstrap/Badge';
+import { React, useEffect, useState } from "react";
+import Button from 'react-bootstrap/Button';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Header from "../components/Header";
 
+function UserRecipeListPage() {
+    const [recipeData, setrecipeData] = useState([]);
 
-function UserRecipeListPage(){
-    const [recipeData,setrecipeData] = useState([]);
+    const deleteHandle=(id)=>{
+        axios
+        .delete('http://localhost:3001/addRecipe/'+id)
+        .then(()=>{
+            fetchRecipeData();
+            alert('삭제 완료!')
+        })
+        .catch(console.error);
+    }
 
-const fetchRecipeData=()=>{
-    axios
-    .get('http://localhost:3001/addRecipe')
-    .then((response)=>{
-        const fetchData = response.data;
-        setrecipeData(fetchData);
-    }).catch(console.error)
-}
+    const fetchRecipeData = () => {
+        axios
+            .get('http://localhost:3001/addRecipe')
+            .then((response) => {
+                const fetchData = response.data;
+                setrecipeData(fetchData);
+            })
+            .catch(console.error);
+    }
 
+    useEffect(() => {
+        fetchRecipeData();
+    }, [])
 
-useEffect(()=>{
-    fetchRecipeData();
-},[])
-
-    return(
+    return (
         <div>
-            <Header/>
+            <Header />
             {
-                   recipeData.map((data,index)=>{
-                    return(
-                        <ListGroup as="ol" >
-                        <ListGroup.Item
-                        as="li"
-                        className="d-flex justify-content-between align-items-start"
-                        >
-                            <div className="ms-2 me-auto">
-                                <div >{data.RCP_NM}</div>
-                                <pre >{data.RCP_PARTS_DTLS}</pre>
-                            </div>
-                        </ListGroup.Item>
-                    </ListGroup>
+                recipeData.map((data, index) => {
+                    return (
+                        <ListGroup key={data.id} as="ol">
+                            <ListGroup.Item as="li" className="d-flex justify-content-between align-items-start">
+                                <div className="ms-2 me-auto">
+                                    <div>{data.RCP_NM}</div>
+                                    <pre>{data.RCP_PARTS_DTLS}</pre>
+                                </div>
+                                <Button style={{ alignSelf: "center" }} variant="danger" onClick={()=>deleteHandle(data.id)} > 삭제하기 </Button>{' '}
+                            </ListGroup.Item>
+                        </ListGroup>
                     )
                 })
             }
         </div>
     )
 }
-
-
-
-
 
 export default UserRecipeListPage;
